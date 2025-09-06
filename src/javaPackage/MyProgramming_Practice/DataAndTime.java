6f02fd428ac41130bffce32622672f1ebebae7a4
vinaybarfa <vinaybarfa54@gmail.com>
2025-09-06 14:55:12 +0530

    Add Current time and Date

--- src/javaPackage/MyProgramming_Practice/DataAndTime.java
+++ src/javaPackage/MyProgramming_Practice/DataAndTime.java
@@ -1,12 +1,22 @@
 package javaPackage.MyProgramming_Practice;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Period;
 
 public class DataAndTime {
 
     public static void main(String[] args) {
     LocalDate dob = LocalDate.of(2007,9,13);
-        System.out.println(Period.between(dob, LocalDate.now()).getDays());
+        System.out.println(Period.between(dob, LocalDate.now()).getYears());
+
+        LocalDate todaysDay = LocalDate.now();
+        System.out.println(todaysDay);
+
+        LocalTime currentTime = LocalTime.now();
+        System.out.println(currentTime);
+
+        System.out.println(System.currentTimeMillis());
+
     }
 }
